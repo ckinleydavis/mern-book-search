b2608c665a5c09caa928e03d740468b54976a674
Kinley Davis <d8tascientist@gmail.com>
2021-10-14 15:09:33 -0400

    fix code deployed to heroku

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const routes = require("./server/routes");
-
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("client/build"));
-app.use(routes);
-
+const mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/mern-book-search", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(require("./server/routes/api"));
+
+const { ApolloServer } = require('apollo-server-express');
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context:authMiddleware
+});
+
+server.applyMiddleware({ app });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
